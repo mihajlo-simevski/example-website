1fddcafaf825ba1fb82e6772c139196d35fe59d0
Mihajlo Simevski <msimevskii@gmail.com>
2023-08-16 17:43:32 +0200

    feat: Adding a service for a fake-store api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
+import { provideHttpClient, withNoXsrfProtection } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, NgbModule, AppRoutingModule],
+  providers: [provideHttpClient(withNoXsrfProtection())],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
