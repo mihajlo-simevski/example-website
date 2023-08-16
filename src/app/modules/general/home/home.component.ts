fa57ec9cda215551bea72cd87955af586abb99ae
Mihajlo Simevski <msimevskii@gmail.com>
2023-08-17 00:40:12 +0200

    feat: Added the about component to the home component

--- src/app/modules/general/home/home.component.ts
+++ src/app/modules/general/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NgbdCarousel } from 'src/app/components/carousel/carousel.component';
+import { AboutComponent } from '../about/about.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgbdCarousel],
+  imports: [NgbdCarousel, AboutComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
