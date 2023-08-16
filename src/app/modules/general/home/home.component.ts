b6922d353a7e358597cd50e36142bb2df8f3aa77
Mihajlo Simevski <msimevskii@gmail.com>
2023-08-16 18:55:19 +0200

    feat: Added a carousel component
    The carousel components consists of a two sections of which the first one is a header, product pricing information and a button to add items to a cart, the second section is an image of the product, there is also a logo placeholder and a cart with an item indicator.

--- src/app/modules/general/home/home.component.ts
+++ src/app/modules/general/home/home.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { NgbdCarousel } from 'src/app/components/carousel/carousel.component';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [NgbdCarousel],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
