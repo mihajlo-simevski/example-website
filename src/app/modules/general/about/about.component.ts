ec849395aa07d8e5aacde7f5df3ac10a183a8397
Mihajlo Simevski <msimevskii@gmail.com>
2023-08-17 01:12:59 +0200

    feat: Added a modal component with some text
    It has a close and X button

--- src/app/modules/general/about/about.component.ts
+++ src/app/modules/general/about/about.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NgbdModalComponent } from 'src/app/components/modal/modal.component';
 
 @Component({
   selector: 'about',
   standalone: true,
+  imports: [NgbdModalComponent],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
 })
