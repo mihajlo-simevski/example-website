1e79568648851a3a20c70894acdfe225243aa3d4
Mihajlo Simevski <msimevskii@gmail.com>
2023-08-17 07:33:56 +0200

    feat: Adding a toast component

--- src/app/modules/general/contact/contact.component.ts
+++ src/app/modules/general/contact/contact.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { ToastContainerComponent } from 'src/app/components/toast/toast.container.component';
+import { ToastService } from 'src/app/services/toast-service';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
+  imports: [ToastContainerComponent],
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
+  constructor(public toastService: ToastService) {}
+  showSuccess() {
+    this.toastService.show('The form is submitted succesfully', {
+      classname: 'bg-success text-light',
+      delay: 10000,
+    });
+  }
 }
