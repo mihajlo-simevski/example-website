f0e2b3e2430f6f971811e215003c76dddd6014f7
Mihajlo Simevski <msimevskii@gmail.com>
2023-08-17 02:06:55 +0200

    feat: Added the table to the about component

--- src/app/modules/general/about/about.component.ts
+++ src/app/modules/general/about/about.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NgbdModalComponent } from 'src/app/components/modal/modal.component';
+import { NgbdTable } from 'src/app/components/table/table.component';
 
 @Component({
   selector: 'about',
   standalone: true,
-  imports: [NgbdModalComponent],
+  imports: [NgbdModalComponent, NgbdTable],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
 })
