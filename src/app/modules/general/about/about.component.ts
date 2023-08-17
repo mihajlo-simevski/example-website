40c38e147bdbe28407a186d299a832f7ffd58f46
Mihajlo Simevski <msimevskii@gmail.com>
2023-08-17 04:11:27 +0200

    feat: Added a contact component

--- src/app/modules/general/about/about.component.ts
+++ src/app/modules/general/about/about.component.ts
@@ -3,12 +3,11 @@ import { NgbdModalComponent } from 'src/app/components/modal/modal.component';
 import { NgbdTable } from 'src/app/components/table/table.component';
 
 @Component({
-  selector: 'about',
+  selector: 'app-about',
   standalone: true,
   imports: [NgbdModalComponent, NgbdTable],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent {
-  constructor() {}
 }
